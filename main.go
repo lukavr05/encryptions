451ef253a8c23b8a6d7618f44cd71ac8a3b1f0b8
lukavr05 <lvrooyen@yahoo.com>
2025-05-04 10:13:55 +0100

    added keystream padding

--- main.go
+++ main.go
@@ -25,6 +25,14 @@ func generateTimeSeed() string {
   return bin_t
 }
 
+func reverseString(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
 // simple XOR function, idc if it already exists
 func xOR(a string, b string) string {
   i := Must(strconv.Atoi(a))
@@ -38,23 +46,34 @@ func xOR(a string, b string) string {
 }
 
 // pad the keystream so that it matches the length of the input string
-func padKeystream(plaintext string, keystream string) string {
-  if (len(keystream) > len(plaintext)) {
-    return keystream[0:len(plaintext)]
+func padKeystream(length int, keystream string) string {
+  if (len(keystream) >= length) {
+    return keystream[0:length]
+  } else {
+    reversed := reverseString(keystream)
+    padded := ""
+
+    useOriginal := true
+
+    for len(padded) < length {
+      if useOriginal {
+        padded += keystream
+      } else {
+        padded += reversed
+      }
+      useOriginal = !useOriginal
+    }
+    return padded[:length]
   }
-
-  if (len(keystream) < len(plaintext)) {
-    new_stream 
-  }
-}
-
-
-func streamCipher(plaintext string, keystream string) string {
-  
-  for 
 }
 
 func main() {
   s := generateTimeSeed()
+  plaintext := "hello"
+  padded := padKeystream((len(plaintext) * 8), s)
+
+  fmt.Printf("Plaintext:        %s\n", plaintext)
+  fmt.Printf("Keystream:        %s\n", s)
+  fmt.Printf("Padded Keystream: %s\n", padded)
 
 }
